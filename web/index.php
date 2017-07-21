1701dc0a5b6720c1911ee3d3edc2bf8253a51a49
Harry Chow <harry.chow@crick.ac.uk>
2017-07-21 17:06:18 +0100

    -added a test template for login. -added autoloading and init in index.php - fixed gitignore

---
+++ web/index.php
@@ -0,0 +1,7 @@
+<?php
+require_once('vendor/autoload.php');
+
+$loader = new \Twig_Loader_Filesystem(__DIR__.'/templates');
+$twig = new \Twig_Environment($loader);
+
+echo $twig->render('login.twig', ['date' => date('Ymd')]);
