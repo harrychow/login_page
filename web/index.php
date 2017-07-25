8fab6ee59a5575dd6a0c659c56ee0ac8a955051a
Harry Chow <harry.chow@crick.ac.uk>
2017-07-25 09:46:34 +0100

    -added symlink of assets (css, js, fonts) to vendor folder - update to composer. symlink created after package download - refactored template, created base template

--- web/index.php
+++ web/index.php
@@ -4,4 +4,10 @@ require_once('vendor/autoload.php');
 $loader = new \Twig_Loader_Filesystem(__DIR__.'/templates');
 $twig = new \Twig_Environment($loader);
 
-echo $twig->render('login.twig', ['date' => date('Ymd')]);
+echo $twig->render('login.twig', []);
+
+?>
+
+
+
+
